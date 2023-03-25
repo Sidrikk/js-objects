a5f5c29b0bbbf66720d75bb93cea52f2d1df14fc
Sidrik <rdix3@yandex.ru>
2023-03-25 16:16:24 +0300

    commit

--- solutions/12-hash-table.js
+++ solutions/12-hash-table.js
@@ -1,14 +1,28 @@
 import crc32 from 'crc-32';
 
 // BEGIN
-const make = () => {
+export const make = () => {
     return [];
 }
 
-const set = (map, key, value) => {
-    if (key in map) {
+export const set = (map, key, value) => {
+    let index = crc32.str(key);
+
+    if (map[index] && map[index][0] != key) {
         return false;
-    } 
+    }
+
+    map[index] = [key, value];
+    return true;
+}
+
+export const get = (map, key, defaultValue = null) => {
+    let index = crc32.str(key);
+
+    if (map[index] && map[index][0] != key) return defaultValue;
+
+    if (map[index]) return map[index][1];
     
+    return defaultValue;
 }
 // END
